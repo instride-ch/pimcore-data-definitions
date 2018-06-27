a8fe74f6fbbfe5f2828b4ba3adf10752eba89618
Dalibor Karlović <dalibor.karlovic@flexolabs.io>
2018-06-27 09:21:45 +0200

    feature: allow Object Resolver to optionally match unpublished objects

--- src/ImportDefinitionsBundle/Form/Type/Interpreter/ObjectResolverType.php
+++ src/ImportDefinitionsBundle/Form/Type/Interpreter/ObjectResolverType.php
@@ -16,6 +16,7 @@ namespace ImportDefinitionsBundle\Form\Type\Interpreter;
 
 use ImportDefinitionsBundle\Form\Type\ClassChoiceType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -28,7 +29,8 @@ class ObjectResolverType extends AbstractType
     {
         $builder
             ->add('class', ClassChoiceType::class)
-            ->add('field', TextType::class);
+            ->add('field', TextType::class)
+            ->add('match_unpublished', CheckboxType::class);
     }
 
     /**

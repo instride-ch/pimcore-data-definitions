f15c77a6d1b41b278b228c94b717050d23b72bea
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-11-27 15:27:05 +0100

    do NOT set Pimcore test otherwise it wants to write into the wrong folder

--- behat-bootstrap.php
+++ behat-bootstrap.php
@@ -4,7 +4,6 @@ require __DIR__ . '/app/AppKernel.php';
 
 define('TESTS_PATH', __DIR__);
 define('PIMCORE_PROJECT_ROOT', __DIR__);
-define('PIMCORE_TEST', true);
 define('PIMCORE_KERNEL_CLASS', '\AppKernel');
 
 \Pimcore\Bootstrap::setProjectRoot();

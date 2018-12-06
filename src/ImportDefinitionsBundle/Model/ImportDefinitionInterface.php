313ef5f596a929b808f983cc37c166f02558d156
ivanmerta <mertaivan@seznam.cz>
2018-12-06 09:00:29 +0000

    CreateVersion
    Adding missing createVersion configuration

--- src/ImportDefinitionsBundle/Model/ImportDefinitionInterface.php
+++ src/ImportDefinitionsBundle/Model/ImportDefinitionInterface.php
@@ -85,6 +85,16 @@ interface ImportDefinitionInterface extends DataDefinitionInterface
      * @param boolean $relocateExistingObjects
      */
     public function setRelocateExistingObjects($relocateExistingObjects);
+	
+	/**
+     * @return mixed
+     */
+    public function getCreateVersion();
+
+    /**
+     * @param boolean $createVersion
+     */
+    public function setCreateVersion($createVersion);
 
     /**
      * @return bool

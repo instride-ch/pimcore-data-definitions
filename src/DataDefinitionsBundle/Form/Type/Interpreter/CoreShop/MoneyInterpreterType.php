3cf124a0267ecb4a7533be2dbef1bba447702082
David Höck <d.hoeck@alpin11.at>
2019-10-08 11:36:08 +0200

    refactored money interpreter type to use callback transformer

--- src/DataDefinitionsBundle/Form/Type/Interpreter/CoreShop/MoneyInterpreterType.php
+++ src/DataDefinitionsBundle/Form/Type/Interpreter/CoreShop/MoneyInterpreterType.php
@@ -4,9 +4,11 @@
 namespace Wvision\Bundle\DataDefinitionsBundle\Form\Type\Interpreter\CoreShop;
 
 
+use CoreShop\Bundle\CurrencyBundle\Form\Type\CurrencyChoiceType;
+use CoreShop\Component\Currency\Model\CurrencyInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 final class MoneyInterpreterType extends AbstractType
@@ -18,6 +20,19 @@ final class MoneyInterpreterType extends AbstractType
     {
         $builder
             ->add('isFloat', CheckboxType::class)
-            ->add('currency', IntegerType::class);
+            ->add('currency', CurrencyChoiceType::class)
+            ->addModelTransformer(new CallbackTransformer(
+                function ($value) {
+                    return $value;
+                },
+                function ($value) {
+
+                    if (isset($value['currency']) && $value['currency'] instanceof CurrencyInterface) {
+                        $value['currency'] = $value['currency']->getId();
+                    }
+
+                    return $value;
+                }
+            ));
     }
 }

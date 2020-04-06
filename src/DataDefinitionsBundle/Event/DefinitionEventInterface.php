1c056ba6b92850e573072737023950d27462978a
Kuba Płaskonka <jplaskonka@divante.pl>
2020-04-06 09:14:17 +0200

    Added licence information to newly created files.

--- src/DataDefinitionsBundle/Event/DefinitionEventInterface.php
+++ src/DataDefinitionsBundle/Event/DefinitionEventInterface.php
@@ -1,8 +1,18 @@
 <?php
-
+/**
+ * Data Definitions.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2020 w-vision AG (https://www.w-vision.ch)
+ * @license    https://github.com/w-vision/DataDefinitions/blob/master/gpl-3.0.txt GNU General Public License version 3 (GPLv3)
+ */
 
 namespace Wvision\Bundle\DataDefinitionsBundle\Event;
-
 
 use Wvision\Bundle\DataDefinitionsBundle\Model\ImportDefinitionInterface;
 
